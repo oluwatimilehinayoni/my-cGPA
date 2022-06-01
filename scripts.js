d578c1b27d4f4be9ada2db667585d60a15493485
developertimi <96679357+developertimi@users.noreply.github.com>
2022-06-01 04:49:22 +0100

    Prince Will

---
+++ scripts.js
@@ -0,0 +1,66 @@
+let courseRow = 5
+semesterRow = 1;
+
+function addCourse() {
+    courseRow += 1;
+    html =
+        '<div class="row" id="courseRow' + courseRow + '">\
+                <div class="col-sm-5 border" >\
+                    <input type="text" placeholder="Course Code">\
+                </div>\
+                <div class="col-sm-4 border">\
+                   <select name="grade_'+ courseRow + '" id="grade" class="">\
+                       <option value="" selected>Grade</option>\
+                            <option value="A">A</option>\
+                            <option value="B">B</option>\
+                            <option value="C">C</option>\
+                            <option value="D">D</option>\
+                            <option value="E">E</option>\
+                            <option value="F">F</option>\
+                        </select>\
+                </div>\
+                <div class="col-sm-3 border">\
+                    <input type="number" name="course_unit_'+ courseRow + '" placeholder="Course Unit" value="' + courseRow + '">\
+                </div>\
+        </div > ';
+
+    var courseDiv = document.getElementById("addCourseDiv")
+    return courseDiv.innerHTML += html
+}
+
+
+function addSemester() {
+    semesterRow += 1;
+    courseRow = courseRow + 1;
+    html =
+        '<div class="row my-4">\
+            <div class="col-sm-9 display-6 me-5">\
+                 Semester '+ semesterRow + '\
+            </div>\
+                <div class="col-sm-1 ms-5">\
+                  x\
+                </div>\
+        </div>\
+                    <div class="row">\
+            <div class="col-sm-5 border">\
+                <input type="text" placeholder="Course Code" class="">\
+            </div>\
+                        <div class="col-sm-4 border">\
+                <select name="grade_'+ courseRow + '" id="grade" class="">\
+                    <option value="" selected>Grade</option>\
+                    <option value="A">A</option>\
+                    <option value="B">B</option>\
+                    <option value="C">C</option>\
+                    <option value="D">D</option>\
+                    <option value="E">E</option>\
+                    <option value="F">F</option>\
+                </select>\
+            </div>\
+<div class="col-sm-3 border">\
+                <input type="number" name="course_unit_'+ courseRow + '" placeholder="Course Unit" value="' + courseRow + '">\
+            </div>\
+        </div> ';
+
+    var courseDiv = document.getElementById("addCourseDiv")
+    return courseDiv.innerHTML += html
+}
