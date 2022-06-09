0bf820d2044de91f2eaf14661a8dcff1c0b070d5
developertimi <96679357+developertimi@users.noreply.github.com>
2022-06-09 17:35:20 +0100

    Prince Will

--- function.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include './controller/gpa.php';
-
-$tpu = 78;
-$tcu = 19;
-
-$gpa = new gpa($tpu, $tcu);
-echo $gpa->getGpa();
--- inc/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    &copy; <?php echo date('Y'); ?> <a href="<?php $_SERVER['PHP_SELF']; ?>">myCGPA</a> | Developed by <a href="http://" target="_blank" rel="noopener noreferrer">Prince Will</a>
-</footer>
-
-<script src="node_modules\bootstrap\dist\js\bootstrap.js"></script>
